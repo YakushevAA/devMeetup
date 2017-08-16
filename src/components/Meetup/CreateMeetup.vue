c4b97d1ef0d18730d4668dfce90fd8f65d136999
Alexandr Yakushev <yakushevmailto@gmail.com>
2017-08-16 23:55:07 +0300

    create meetup added

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <p>The Create Meetup Page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12>
+        <h4 class="primary--text">Create a new meetup</h4>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
